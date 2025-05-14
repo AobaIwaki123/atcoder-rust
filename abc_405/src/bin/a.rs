4126e4a9f9a02a3c40648709cdb4cc47d2a3e648
AobaIwaki <aobaiwaki@gmail.com>
2025-05-15 01:22:05 +0900

    補完

--- abc_405/src/bin/a.rs
+++ abc_405/src/bin/a.rs
@@ -1,14 +1,14 @@
 use std::io::{self, Read};
 
 fn main() {
-  let mut buf = String::new();
+  let mut buf: String  = String::new();
   io::stdin().read_to_string(&mut buf).unwrap();
 
-  let mut iter = buf.split_whitespace();
+  let mut iter: std::str::SplitWhitespace<'_> = buf.split_whitespace();
   let r: i32 = iter.next().unwrap().parse().unwrap();
   let x: i32 = iter.next().unwrap().parse().unwrap();
 
-  let ok = match x {
+  let ok: bool = match x {
     1 => 1600 <= r && r <= 2999,
     2 => 1200 <= r && r <= 2399,
     _ => false,
