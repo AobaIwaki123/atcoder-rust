21d93f6a87bc799dd2ada290cb15186ce8fdd240
AobaIwaki <aobaiwaki@gmail.com>
2025-06-07 18:30:52 +0900

    fix: GPTとだけではダメだったので、模範解答をRust化

--- abc_406/src/c.rs
+++ abc_406/src/c.rs
@@ -10,43 +10,23 @@ fn main() {
         .map(|_| iter.next().unwrap().parse::<i32>().unwrap())
         .collect();
 
-    let mut diff = vec![0; n - 1];
+    let mut rle: Vec<(char, usize)> = vec![];
+
     for i in 0..n - 1 {
-        if p[i] < p[i + 1] {
-            diff[i] = 1;
+        let dir = if p[i] < p[i + 1] { '<' } else { '>' };
+        if rle.is_empty() || rle.last().unwrap().0 != dir {
+            rle.push((dir, 1));
         } else {
-            diff[i] = -1;
+            rle.last_mut().unwrap().1 += 1;
         }
     }
 
-    let mut peaks = vec![];
-    let mut valleys = vec![];
-    for i in 1..n - 1 {
-        if p[i - 1] < p[i] && p[i] > p[i + 1] {
-            peaks.push(i);
+    let mut ans: i64 = 0;
+
+    for i in 1..rle.len() - 1 {
+        if rle[i].0 == '>' {
+            ans += (rle[i - 1].1 as i64) * (rle[i + 1].1 as i64);
         }
-        if p[i - 1] > p[i] && p[i] < p[i + 1] {
-            valleys.push(i);
-        }
-    }
-
-    let mut ans = 0;
-    let mut l = 0;
-    while l <= n - 4 {
-        let mut r = l + 3;
-
-        while r < n {
-            let peak_cnt = peaks.iter().filter(|&&x| l < x && x < r).count();
-            let valley_cnt = valleys.iter().filter(|&&x| l < x && x < r).count();
-
-            if p[l] < p[l + 1] && peak_cnt == 1 && valley_cnt == 1 {
-                ans += 1;
-            }
-
-            r += 1;
-        }
-
-        l += 1;
     }
 
     println!("{}", ans);
