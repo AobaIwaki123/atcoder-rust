c93473506767d794e57a1c0ffd16a6665d0a3739
AobaIwaki <aobaiwaki@gmail.com>
2025-06-08 16:45:39 +0900

    add: b問題を実装

--- abc_408/src/b.rs
+++ abc_408/src/b.rs
@@ -1,1 +1,21 @@
+use std::collections::BTreeSet;
+use std::io::{self, Read};
 
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut iter = buf.split_whitespace();
+
+    // 1 <= n <= 100
+    let n: usize = iter.next().unwrap().parse().unwrap();
+
+    // 1 <= a_i <= 100
+
+    let a: BTreeSet<i32> = (0..n)
+        .map(|_| iter.next().unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    println!("{}", a.len());
+    let vec: Vec<_> = a.iter().map(|x| x.to_string()).collect();
+    println!("{}", vec.join(" "));
+}
