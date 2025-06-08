e1c6aa0801db800479eda7e48ecaf9aa76e48ad3
AobaIwaki <aobaiwaki@gmail.com>
2025-06-08 16:07:22 +0900

    add: a問題を実装 (1問だけ解けなかったので模範解答を見て微修正)

--- abc_408/src/a.rs
+++ abc_408/src/a.rs
@@ -1,3 +1,30 @@
+use std::io::{self, Read};
+
 fn main() {
-    println!("Hello, world!");
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut iter = buf.split_whitespace();
+
+    // 1 <= n <= 100
+    let n: usize = iter.next().unwrap().parse().unwrap();
+    // 1 <= s <= 100
+    let s: i32 = iter.next().unwrap().parse().unwrap();
+    // 1 <= t_i <= 1000
+    let t: Vec<i32> = (0..n)
+        .map(|_| iter.next().unwrap().parse::<i32>().unwrap())
+        .collect();
+
+    if t[0] > s {
+        println!("No");
+        return;
+    }
+
+    for i in 1..n {
+        if t[i] - t[i - 1] > s {
+            println!("No");
+            return;
+        }
+    }
+
+    println!("Yes");
 }
