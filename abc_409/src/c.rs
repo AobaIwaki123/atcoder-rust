29df6bd2eaab6e25335a6235e98bb9671b4bff93
AobaIwaki <aobaiwaki@gmail.com>
2025-07-05 19:20:33 +0900

    add: c問題を解いた

--- abc_409/src/c.rs
+++ abc_409/src/c.rs
@@ -1,3 +1,42 @@
+use std::io::{self, BufRead};
+
 fn main() {
-    println!("Hello, world!");
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+
+    // N, Lの入力
+    let first_line = lines.next().unwrap().unwrap();
+    let mut iter = first_line.split_whitespace();
+    let n: usize = iter.next().unwrap().parse().unwrap();
+    let l: usize = iter.next().unwrap().parse().unwrap();
+
+    if l % 3 != 0 {
+        println!("0");
+        return;
+    }
+
+    // dの入力
+    let d_line = lines.next().unwrap().unwrap();
+    let d: Vec<usize> = d_line
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect();
+
+    let mut x = 0;
+    let mut cnt = vec![0_usize; l];
+
+    for i in 0..n {
+        if i != 0 {
+            x += d[i - 1];
+        }
+        x %= l;
+        cnt[x] += 1;
+    }
+
+    let mut ans = 0_usize;
+    for i in 0..(l / 3) {
+        ans += cnt[i] * cnt[i + l / 3] * cnt[i + 2 * l / 3];
+    }
+
+    println!("{}", ans);
 }
