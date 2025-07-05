a92aa815098b5371347078dda3cf8fb70e192e18
AobaIwaki <aobaiwaki@gmail.com>
2025-07-05 18:24:57 +0900

    add: a問題を解いてみた

---
+++ abc_409/src/a.rs
@@ -0,0 +1,25 @@
+use std::io::{self, Read};
+
+fn main() {
+    let mut buf = String::new();
+    io::stdin().read_to_string(&mut buf).unwrap();
+    let mut iter = buf.split_whitespace();
+
+    let n: usize = iter.next().unwrap().parse().unwrap();
+    let t: String = iter.next().unwrap().to_string();
+    let a: String = iter.next().unwrap().to_string();
+
+    let mut ans = false;
+
+    for i in 0..n {
+        if t.chars().nth(i) == a.chars().nth(i) {
+            ans = true;
+        }
+    }
+
+    if ans {
+        println!("Yes");
+    } else {
+        println!("No");
+    }
+}   
