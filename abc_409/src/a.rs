18b967da55817e441250df6f665a240405e99589
AobaIwaki <aobaiwaki@gmail.com>
2025-07-05 18:34:27 +0900

    refactor

--- abc_409/src/a.rs
+++ abc_409/src/a.rs
@@ -2,24 +2,14 @@ use std::io::{self, Read};
 
 fn main() {
     let mut buf = String::new();
-    io::stdin().read_to_string(&mut buf).unwrap();
+    io::stdin().read_to_string(&mut buf).expect("Failed to read input");
     let mut iter = buf.split_whitespace();
 
-    let n: usize = iter.next().unwrap().parse().unwrap();
-    let t: String = iter.next().unwrap().to_string();
-    let a: String = iter.next().unwrap().to_string();
+    let n: usize = iter.next().expect("Missing N").parse().expect("Invalid N");
+    let t = iter.next().expect("Missing T").chars().collect::<Vec<_>>();
+    let a = iter.next().expect("Missing A").chars().collect::<Vec<_>>();
 
-    let mut ans = false;
+    let ans = t.iter().zip(a.iter()).any(|(&c1, &c2)| c1 == 'o' && c2 == 'o');
 
-    for i in 0..n {
-        if t.chars().nth(i).unwrap() == 'o' && a.chars().nth(i).unwrap() == 'o' {
-            ans = true;
-        }
-    }
-
-    if ans {
-        println!("Yes");
-    } else {
-        println!("No");
-    }
+    println!("{}", if ans { "Yes" } else { "No" });
 }   
